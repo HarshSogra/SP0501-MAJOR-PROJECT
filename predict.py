f6aa85c0da45dcbf98da5748e5b31c377bcdaa52
Harsh Sogra <harshsogra@gmail.com>
2026-02-16 01:02:39 +0530

    Change from image processing to real-time video detection
    we have changed the input taking process now it will be live video throught webcam it can take data from directly there live

--- predict.py
+++ predict.py
@@ -8,23 +8,15 @@ model = pickle.load(open("model.pkl", "rb"))
 
 # Initialize MediaPipe
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=True)
+hands = mp_hands.Hands()
+cap = cv2.VideoCapture(0)
 
-# Give image path here
-image_path = "test.jpg"   # 🔹 Change this to your image file name
+while True:
+    ret,frame = cap.read()
+    frame = cv2.flip(frame,1)
+    rgb = cv2.cvtcolor(frame,cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb)
 
-# Read image
-image = cv2.imread(image_path)
-
-if image is None:
-    print("Error: Image not found!")
-    exit()
-
-# Convert BGR to RGB
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# Process image
-results = hands.process(rgb)
 
 if results.multi_hand_landmarks:
     for handLms in results.multi_hand_landmarks:
@@ -41,13 +33,10 @@ if results.multi_hand_landmarks:
         cv2.putText(image, prediction, (50, 50),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1, (0, 255, 0), 2)
-
-    print("Prediction:", prediction)
-
-else:
-    print("No hand detected!")
-
-# Show output image
-cv2.imshow("Sign Detection", image)
-cv2.waitKey(0)
+    cv2.imshow("Sign Detection",frame)
+    if cv2.waitkey(1) & 0xFF == ord('q'):
+        break
+        
+cap.release
 cv2.destroyAllWindows()
+
